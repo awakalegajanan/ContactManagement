7189002fa965d8a48d6cad12bcda35757cf98dec
awakalegajanan <34919512+awakalegajanan@users.noreply.github.com>
2021-10-04 01:45:05 +0530

    Add files via upload

---
+++ OnionContactManagementSolution.Logging/Logger.cs
@@ -0,0 +1,25 @@
+﻿using OnionContactManagementSolution.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionContactManagementSolution.Logging
+{
+    public class Logger : ILoggerInterface
+    {
+        public void Debug(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Error(Exception exception, string message)
+        {
+            Console.WriteLine($"Exception details are: {message} and stack trace : {exception.StackTrace}");
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
