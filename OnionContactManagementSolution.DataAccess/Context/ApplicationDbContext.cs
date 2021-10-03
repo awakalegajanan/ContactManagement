f29c9707454349af663b1d60b53a8a97c010175f
awakalegajanan <34919512+awakalegajanan@users.noreply.github.com>
2021-10-04 01:44:37 +0530

    Add files via upload

---
+++ OnionContactManagementSolution.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionContactManagementSolution.Core.Interfaces;
+using OnionContactManagementSolution.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionContactManagementSolution.DataAccess.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public int SaveChanges()
+        {
+            return base.SaveChanges();            
+        }
+    }
+}
---
+++ OnionContactManagementSolution.DataAccess/Migrations/20211003083338_contact31021.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnionContactManagementSolution.DataAccess.Migrations
+{
+    public partial class contact31021 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
