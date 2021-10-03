406558a6e4c6c1b59bc87d39d34392c1a787e5a1
awakalegajanan <34919512+awakalegajanan@users.noreply.github.com>
2021-10-04 02:02:24 +0530

    Add files via upload

---
+++ OnionContactManagementSolution.WebAPI/Exception/HttpStatusCodeException.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OnionContactManagementSolution.WebAPI.Exception
+{
+    public class HttpStatusCodeException : System.Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string ContentType { get; set; } = @"text/plain";
+        public string Message { get; set; }
+
+        public HttpStatusCodeException(HttpStatusCode statusCode)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCodeException(HttpStatusCode statusCode, string message)
+            : base(message)
+        {
+            this.StatusCode = statusCode;
+        }
+
+        public HttpStatusCodeException(HttpStatusCode statusCode, System.Exception inner)
+            : this(statusCode, inner.ToString()) { }
+
+        public HttpStatusCodeException(HttpStatusCode statusCode, JObject errorObject)
+            : this(statusCode, errorObject.ToString())
+        {
+            this.ContentType = @"application/json";
+        }
+
+    }
+}
